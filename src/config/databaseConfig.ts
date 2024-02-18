7ecd9935d442da991e49046484bee150cd61672c
soshal dahal <soshal.dahal@kotuko.it>
2024-02-18 20:37:18 +0545

    fix: database cred updated

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -1,6 +1,6 @@
 export const databaseConfig = {
-  databaseUser: process.env.POSTGRES_USER ?? "postgres.baqysrgxzjkatpbfpdaf",
-  databasePassword: process.env.POSTGRES_PASSWORD ?? "Tcs6hHc8g9eKQEFv",
+  databaseUser: process.env.POSTGRES_USER ?? "postgres.ipncvxkfahdqwpsxxlbr",
+  databasePassword: process.env.POSTGRES_PASSWORD ?? "maizedisease9870",
   databaseName: process.env.POSTGRES_DB ?? "postgres",
   databaseHost:
     process.env.POSTGRES_HOST ?? "aws-0-ap-southeast-1.pooler.supabase.com",
